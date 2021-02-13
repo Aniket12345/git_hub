a135a82d39cee4256ece915eb77e7e64f03383c5
Aniket12345 <any.fondekar@gmail.com>
2021-02-13 22:14:30 +0530

    Millis implemented using TIM4

---
+++ Blink/inc/stm8_millis.h
@@ -0,0 +1,16 @@
+/**
+  *********************************************************************
+  * @file    stm8_millis.h
+  * @author  Aniket Fondekaar
+  * @date    13 Feb 2021
+  * @brief   timer (millis) 
+  *********************************************************************
+**/
+#include "stm8.h"
+#ifndef STM8_TIM4_H
+#define STM8_TIM4_H
+
+void Init_Timer4(void);
+void Setup_TIM4(void);
+
+#endif
--- Blink/src/blink.c
+++ Blink/src/blink.c
@@ -7,6 +7,7 @@
   *********************************************************************
 **/
 #include <stdint.h>
+#include "stm8_millis.h"
 #include "stm8.h"
 
 /* Build in LED is in pin B5 (STM8S103 board) or D3 (STM8S003F3 board) */
@@ -18,14 +19,17 @@
 #define LED_PIN     PIN3
 #endif
 
-/* Simple busy loop delay */
-void delay(unsigned long count) {
-    while (count--)
-        nop();
+// ISR for tim4 overflow
+void timer_isr(void) __interrupt(TIM4_OVR_UIF_IRQ)
+{
+    PB_ODR ^= (1 << 5);
+    TIM4_SR &= ~(TIM_SR1_UIF);
 }
+
 
 int main(void)
 {
+    disableInterrupts();
     /* Set clock to full speed (16 Mhz) */
     CLK_CKDIVR = 0;
 
@@ -35,12 +39,12 @@ int main(void)
     // Set pin as "Push-pull"
     PORT(LED_PORT, CR1)  |= LED_PIN; // i.e. PB_CR1 |= (1 << 5);
 
-	while(1) {
-        // LED on
-        PORT(LED_PORT, ODR) |= LED_PIN; // PB_ODR |= (1 << 5);
-        delay(100000L);
-        // LED off
-        PORT(LED_PORT, ODR) &= ~LED_PIN; // PB_ODR &= ~(1 << 5);
-        delay(300000L);
-    }
+    Init_Timer4();
+    Setup_TIM4();
+    enableInterrupts();
+
+	while(1) 
+    {
+	wfi();
+   }
 }
